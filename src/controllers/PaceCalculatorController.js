6d4738f3d827fe38d6220aa8b0345cafe39e6c13
Waldemar Scigocki <waldemar.scigocki@psat.pl>
2018-09-06 11:37:33 +0200

    Obliczanie prędkości w kalkulatorze tempa

--- src/controllers/PaceCalculatorController.js
+++ src/controllers/PaceCalculatorController.js
@@ -8,8 +8,9 @@ function PaceCalculatorController($log, calculatorService, timeService) {
   ctrl.seconds = 0;
   ctrl.totalSeconds = 0;
   ctrl.peace = 0;
+  ctrl.speed = 0;
 
-  ctrl.calculate = function () {
+  ctrl.calculatePeace = function () {
     var peace = calculatorService.calculatePeace(ctrl.totalSeconds, ctrl.distance);
     if (peace == undefined) {
       ctrl.peace = "?";
@@ -19,10 +20,26 @@ function PaceCalculatorController($log, calculatorService, timeService) {
     }
   }
 
-  ctrl.onTotalSecondsChange = function (totalSeconds) {
-    ctrl.totalSeconds = totalSeconds;
-    ctrl.calculate();
+  ctrl.calculateSpeed = function () {
+    var speed = calculatorService.calculateSpeed(ctrl.totalSeconds, ctrl.distance);
+    if (speed == undefined) {
+      ctrl.speed = "?";
+    }
+    else {
+      ctrl.speed = speed;
+    }
   }
 
+  ctrl.onTotalSecondsChange = function (totalSeconds) {
+    ctrl.totalSeconds = totalSeconds;
+    ctrl.calculatePeace();
+    ctrl.calculateSpeed();
+  }
+
+  ctrl.onDistanseChange = function() {
+    ctrl.calculatePeace();
+    ctrl.calculateSpeed();
+    
+  }
 
 }
--- src/services/CalculatorService.js
+++ src/services/CalculatorService.js
@@ -4,4 +4,8 @@ function CalculatorService() {
   service.calculatePeace = function(totalSeconds, distance) {
     return (totalSeconds / 60) / distance;
   }
+
+  service.calculateSpeed = function(totalSeconds, distance) {
+    return distance / (totalSeconds / 60 / 60);
+  }
 }
